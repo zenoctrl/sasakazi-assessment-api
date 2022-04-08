24b25cb2b284f0c2005abb80cdd084142d231bd9
zenocodes <azronebrian@gmail.com>
2022-04-08 06:29:16 +0300

    added functions to add & remove choice

--- public/js/add_choice.js
+++ public/js/add_choice.js
@@ -1,10 +1,26 @@
-let span = document.querySelector('span')
+let span = document.querySelector('#add-choice')
 let choices = document.querySelector('#choices')
+let deleteChoiceBtns = document.querySelectorAll('.delete-choice')
+
+const deleteChoice = (e) => {
+    let choice = e.target.parentNode.parentElement
+    choices.removeChild(choice)
+}
 
 span.addEventListener('click', () => {
     let div = document.createElement('div')
     div.className = 'choice'
-    let input = document.createElement('<input type="text" name="option[]" id="">')
+    let input = document.createElement("input")
+    input.setAttribute('type', 'text')
+    input.name = 'options'
+    let span = document.createElement('span')
+    span.innerHTML = '<img src="/images/x-circle.svg" alt="">'
+    div.append(span)
+    span.addEventListener('click', deleteChoice)
     div.append(input)
     choices.append(div)
+})
+
+deleteChoiceBtns.forEach(deleteChoiceBtn => {
+    deleteChoiceBtn.addEventListener('click', deleteChoice)
 })
